04ae388085faf369763021214d2efd7ecd1fb170
Ethan Raque <ethanraque@gmail.com>
2022-01-29 00:13:27 -0500

    add bubble_sort function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, World!");
+}
+
+fn bubble_sort(array: &mut [i32]) {
+    let mut swapped: bool = true;
+    while swapped {
+        swapped = false;
+        for i in 1..array.len() {
+            if array[i-1] > array[i] {
+                array.swap(i-1, i);
+                swapped = true;
+            }
+        }
+    }
 }
