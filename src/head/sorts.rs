6ee3d6e78c4a653ab86581a65ac78c8c0364a33a
Ethan Raque <ethanraque@gmail.com>
2022-01-31 13:16:09 -0500

    add merge sort code

--- src/head/sorts.rs
+++ src/head/sorts.rs
@@ -12,3 +12,52 @@ pub fn bubble_sort(vec: &Vec<i32>) -> Vec<i32> {
     }
     vec_new
 }
+
+pub mod ModMerge {
+    pub fn merge_sort(vec: &Vec<i32>) -> Vec<i32> {
+        if vec.len() < 2 {
+            vec.to_vec()
+        } else {
+            let size = vec.len() / 2;
+            let left = merge_sort(&vec[0..size].to_vec());
+            let right = merge_sort(&vec[size..].to_vec());
+            let merged = merge(&left, &right);
+
+            merged
+        }
+    }
+
+    fn merge(left: &Vec<i32>, right: &Vec<i32>) -> Vec<i32> {
+        let mut l = 0;
+        let mut r = 0;
+        let mut merged: Vec<i32> = Vec::new();
+
+        while l < left.len() && r < right.len() {
+            if left[l] < right[r] {
+                merged.push(left[l]);
+                l = l + 1;
+            } else {
+                merged.push(right[r]);
+                r = r + 1;
+            }
+        }
+        
+        if l < left.len() {
+            while l < left.len() {
+                merged.push(left[l]);
+                l = l +1;
+            }
+        }
+
+        if r < right.len() {
+            while r < right.len() {
+                merged.push(right[r]);
+                r = r + 1;
+            }
+        }
+        
+        merged
+    }
+}
+
+
