8c6cbb421bddd055f5de1cdc4a8c43f5f3216dbe
Ethan Raque <ethanraque@gmail.com>
2023-03-06 14:59:49 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 pub mod sorts;
 pub use bubble::bubble_sort;
-pub use sorts::mod_merge::merge_sort_float;
-pub use sorts::mod_merge::merge_sort;
+pub use merge::mod_merge::merge_sort_float;
+pub use merge::mod_merge::merge_sort;
