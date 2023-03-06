bde46cb6ee9be00af9bb0b4047869144089413ae
Ethan Raque <ethanraque@gmail.com>
2023-03-06 14:59:29 -0500

    Update and rename sorts.rs to merge.rs

--- src/merge.rs
+++ src/merge.rs
@@ -1,18 +1,3 @@
-pub fn bubble_sort(vec: &Vec<i32>) -> Vec<i32> {
-    let mut vec_new = vec.to_vec();
-    let mut swapped: bool = true;
-    while swapped {
-        swapped = false;
-        for i in 1..vec_new.len() {
-            if vec_new[i-1] > vec_new[i] {
-                vec_new.swap(i-1, i);
-                swapped = true;
-            }
-        }
-    }
-    vec_new
-}
-
 pub mod mod_merge {
     use std::sync::mpsc;
     use std::thread;
